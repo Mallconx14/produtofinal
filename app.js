b61f0b7113555d2e9334b93e16b0c82d469a014f
Mallconx14 <mallconx14@gmail.com>
2026-03-09 10:32:07 -0300

    adição dos app.use

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ app.use ((req, res, next) => {
         return res.status(200).json({})
     }
     next()
-})
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not found...');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
